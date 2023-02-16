3c209babf889cc2388669da70e8da22b1e292eb6
Samub96 <samuel.d.barona@gmai.com>
2023-02-16 15:15:52 -0500

    probando otro ide

--- Seguimineto2/src/model/Node.java
+++ Seguimineto2/src/model/Node.java
@@ -4,7 +4,24 @@ public class Node {
 	
 	private int value;
 	private int next;
-	
-	
 
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public Node(int value) {
+		this.value = value;
+	}
+
+	@java.lang.Override
+	public java.lang.String toString() {
+		return "Node{" +
+				"value=" + value +
+				", next=" + next +
+				'}';
+	}
 }
